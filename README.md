26400be5317fa0f5be0b017356708b949d290bf4
serhiibilous <serhiybilouss@gmail.com>
2021-02-26 14:52:24 +0200

    Finished with style guide and common styles Ready to development

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# berezha-dev
+# Berezha-dev
 
 To run webpack watch mode, run from terminal this command - `npm run watch`
+
+Use BEM methodology as main thing how write CSS - 
+[BEM](https://en.bem.info/methodology/quick-start/)
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 console.log('It works')
+const HEIGHT_FIRST_BLOCK = 100
+
+document.addEventListener('scroll', () => {
+  const header = document.getElementById('header')
+  if (window.scrollY > HEIGHT_FIRST_BLOCK) {
+    header.classList.add('header_fixed')
+  } else {
+    header.classList.remove('header_fixed')
+  }
+})
