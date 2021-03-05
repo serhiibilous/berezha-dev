d97d074ee5dbc781d4e7ef8ff4fe9d984bbc575e
serhiibilous <serhiybilouss@gmail.com>
2021-03-05 18:40:34 +0200

    Mobile Header

--- src/main.js
+++ src/main.js
@@ -9,3 +9,12 @@ document.addEventListener('scroll', () => {
     header.classList.remove('header_fixed')
   }
 })
+
+const mobileNavButton = document.getElementById('mobileNavButton')
+const headerNavigation = document.getElementById('headerNavigation')
+mobileNavButton.addEventListener('click', (e) => {
+  const el = e.currentTarget
+  el.classList.toggle('open-navigation')
+  headerNavigation.classList.toggle('open-navigation')
+  console.log(el)
+})
