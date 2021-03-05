d9238202436b4ddc099c0763cf2a286f2c924adb
serhiibilous <serhiybilouss@gmail.com>
2021-03-05 20:00:08 +0200

    Added FAQ section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 console.log('It works')
 const HEIGHT_FIRST_BLOCK = 100
+const body = document.body
 
 document.addEventListener('scroll', () => {
   const header = document.getElementById('header')
@@ -10,11 +11,22 @@ document.addEventListener('scroll', () => {
   }
 })
 
+// Mobile Menu
 const mobileNavButton = document.getElementById('mobileNavButton')
 const headerNavigation = document.getElementById('headerNavigation')
 mobileNavButton.addEventListener('click', (e) => {
   const el = e.currentTarget
   el.classList.toggle('open-navigation')
   headerNavigation.classList.toggle('open-navigation')
+  body.classList.toggle('overflowed')
   console.log(el)
+})
+
+// FAQ
+const faqItems = document.querySelectorAll('.faq__control')
+faqItems.forEach((faq) => {
+  faq.addEventListener('click', (e) => {
+    const el = e.currentTarget
+    el.closest('.faq__item').classList.toggle('faq__item_open')
+  })
 })
