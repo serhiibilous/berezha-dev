6bdcc0da7b2a6ce2861324276955e95c99a5e02a
serhiibilous <serhiybilouss@gmail.com>
2021-04-13 22:50:04 +0300

    Update Training form

--- src/js/form/training/index.js
+++ src/js/form/training/index.js
@@ -6,4 +6,4 @@ const trainingForm = document.getElementById('trainingForm')
 if (trainingForm) ReactDOM.render(<TrainingForm />, trainingForm)
 
 const trainingFormSecurity = document.getElementById('trainingFormSecurity')
-if (trainingFormSecurity) ReactDOM.render(<TrainingForm isSecurityTraining={true} />, trainingForm)
+if (trainingFormSecurity) ReactDOM.render(<TrainingForm isSecurityTraining={true} />, trainingFormSecurity)
