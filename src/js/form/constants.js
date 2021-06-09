4a7e8e2c7c47af73ffe61eb6a19018d0b87ae6d8
serhiibilous <serhiybilouss@gmail.com>
2021-06-09 10:05:41 +0300

    Updated pentester training form

--- src/js/form/constants.js
+++ src/js/form/constants.js
@@ -9,3 +9,13 @@ export const PDF_REPORT = {
 }
 
 export const contactEmail = 'hello@bsg.tech'
+
+// Prices
+export const fullProgramPrice = '26 000'
+export const fullProgramPriceWithSale = '24 000'
+export const fullProgramPriceWithSaleAndStudent = '18 000'
+export const fullProgramPriceWithSaleAndMilitaryVeteran = '15 600'
+
+export const partProgramPrice = '13 000'
+export const partProgramPriceAndStudent = '9 750'
+export const partProgramPriceMilitaryVeteran = '7 800'
